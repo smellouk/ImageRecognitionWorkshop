149a5b103b6b6a31913bfdea0f5fadf364364db2
Sidali Mellouk <s.mellouk@outlook.com>
2018-05-25 02:37:25 +0200

    Add View Pin Cropper

---
+++ app/src/main/java/io/nyris/metamarathon/PinViewCropper/OnPinClickListener.java
@@ -0,0 +1,14 @@
+/*
+ * OnPinClickListener.java
+ * Created or changed by nyris GmbH
+ * Copyright © 2018 nyris GmbH. All rights reserved.
+*/
+
+
+package io.nyris.metamarathon.PinViewCropper;
+
+import android.graphics.RectF;
+
+public interface OnPinClickListener {
+    void pinClick(RectF objectProposal);
+}
